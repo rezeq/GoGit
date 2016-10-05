926863a45f69e7e9ed0c4834512ea12e8e37bd41
aashgar <aashgar@yahoo.com>
2016-10-05 10:59:20 +0300

    Create ARTStudent.java

---
+++ src/Facade/ARTStudent.java
@@ -0,0 +1,14 @@
+package Facade;
+
+/**
+ *
+ * @author aashgar
+ */
+public class ARTStudent implements Studnet{
+
+    @Override
+    public void show() {
+        System.out.println("ART Student...");
+    }
+    
+}
